cdfc3597fd42c4e9347e11979fdc91c6d8cfc5ec
Joe <joefuller042@gmail.com>
2023-07-17 18:38:19 +0100

    show answer on click

--- app/components/answerTile.js
+++ app/components/answerTile.js
@@ -1,7 +1,16 @@
+import { useState } from "react";
+
 export default function AnswerTile({ questionObject }) {
+  const [showAnswer, setShowAnswer] = useState(false);
+
   return (
-    <div className="w-40 h-24 bg-blue-500 text-white text-center flex items-center justify-center">
-      {questionObject.clue_value}
+    <div
+      className="w-40 h-24 bg-blue-500 text-white text-center flex items-center justify-center"
+      onClick={() => {
+        setShowAnswer(!showAnswer);
+      }}
+    >
+      {showAnswer ? questionObject.answer : questionObject.clue_value}
     </div>
   );
 }

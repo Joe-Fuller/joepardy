1b74e95cbf5f357b40ddb714eb5cb85ab9021890
Joe <joefuller042@gmail.com>
2023-07-17 14:20:21 +0100

    add extra row for categories

--- app/components/jeopardyBoard.js
+++ app/components/jeopardyBoard.js
@@ -4,8 +4,8 @@ export default function JeopardyBoard() {
   return (
     <div className="grid grid-cols-5 gap-4 p-4">
       {Array.from({ length: 5 }, (_, rowIndex) => (
-        <div className="grid grid-rows-5 gap-4 p-4" key={rowIndex}>
-          {Array.from({ length: 5 }, (_, colIndex) => (
+        <div className="grid grid-rows-6 gap-4 p-4" key={rowIndex}>
+          {Array.from({ length: 6 }, (_, colIndex) => (
             <div
               className="w-40 h-24 bg-blue-500 text-white flex items-center justify-center text-lg font-bold"
               key={colIndex}

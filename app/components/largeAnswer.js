39a27d2d5dabcc720756cc57cc478c8e76fdc896
Joe <joefuller042@gmail.com>
2023-07-18 13:01:14 +0100

    add timer to questions

--- app/components/largeAnswer.js
+++ app/components/largeAnswer.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function LargeAnswer({
   answer,
@@ -9,6 +9,34 @@ export default function LargeAnswer({
   incrementQuestionsAnswered,
 }) {
   const [guess, setGuess] = useState("");
+  const [timeRemaining, setTimeRemaining] = useState(15);
+  const [timerActive, setTimerActive] = useState(false);
+
+  useEffect(() => {
+    startTimer();
+  }, []);
+
+  useEffect(() => {
+    setTimeRemaining(15);
+  }, [timerActive]);
+
+  const startTimer = () => {
+    setTimerActive(true);
+
+    const timerId = setInterval(() => {
+      setTimeRemaining((prevTimeRemaining) => prevTimeRemaining - 1);
+    }, 1000);
+
+    setTimeout(() => {
+      clearInterval(timerId);
+      setTimerActive(false);
+      handleTimeOut();
+    }, 15000);
+  };
+
+  const handleTimeOut = () => {
+    console.log("TIMEOUT");
+  };
 
   const checkGuess = (guessToCheck) => {
     console.log(guessToCheck);
@@ -26,10 +54,15 @@ export default function LargeAnswer({
     }
   };
 
+  const handleClick = () => {
+    incrementQuestionsAnswered(1);
+    hideAnswer();
+  };
+
   return (
     <div className="fixed top-0 left-0 w-screen h-screen flex flex-col items-center justify-center">
-      <div className="bg-jeopardy-blue w-4/5 h-3/5 p-40 flex flex-col items-center justify-center text-7xl">
-        {answer}
+      <div className="bg-jeopardy-blue w-4/5 h-3/5 p-10 flex flex-col items-center justify-center text-7xl">
+        {timerActive ? answer : question}
       </div>
       <input
         className="mt-4 px-4 py-2 rounded-md border border-gray-400 focus:outline-none text-black"
@@ -43,6 +76,16 @@ export default function LargeAnswer({
         // onKeyDown={handleKeyDown}
         autoFocus
       />
+      {timerActive ? (
+        <p className="fixed bottom-32 right-20">{timeRemaining}</p>
+      ) : (
+        <button
+          className="fixed bottom-20 bg-jeopardy-blue rounded-md"
+          onClick={handleClick}
+        >
+          Okay
+        </button>
+      )}
     </div>
   );
 }

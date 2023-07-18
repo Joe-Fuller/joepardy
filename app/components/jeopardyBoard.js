c7ce816c7f29441e8a60a71fb8429007b0943bb9
Joe <joefuller042@gmail.com>
2023-07-18 12:40:39 +0100

    increase 'loading' text size

--- app/components/jeopardyBoard.js
+++ app/components/jeopardyBoard.js
@@ -16,7 +16,7 @@ export default function JeopardyBoard({
               // If questions is empty, return a placeholder or loading state
               return (
                 <div
-                  className="w-40 h-24 bg-jeopardy-blue text-jeopardy-yellow flex items-center justify-center"
+                  className="w-40 h-24 bg-jeopardy-blue text-jeopardy-yellow flex items-center justify-center text-3xl"
                   key={rowIndex}
                 >
                   Loading...

23fa1d8f27c4b74aa98456279fad7113653354c4
Joe <joefuller042@gmail.com>
2023-07-19 13:45:06 +0100

    change colours

--- app/components/answerTile.js
+++ app/components/answerTile.js
@@ -23,7 +23,7 @@ export default function AnswerTile({
 
   return (
     <div
-      className="w-40 h-24 bg-jeopardy-blue text-white text-center text-5xl flex items-center justify-center"
+      className="w-40 h-24 bg-jeopardy-blue text-jeopardy-yellow text-center text-5xl flex items-center justify-center"
       onClick={() => {
         if (!questionAnswered) {
           setShowAnswer(true);
